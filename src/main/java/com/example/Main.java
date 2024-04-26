ff67b01f0501f30a0a4ee538e9a67cb9368c697f
Adria <adma769@vidalibarraquer.net>
2024-04-26 12:33:08 +0200

    testeando los pusheos

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -5,7 +5,15 @@ public class Main {
         System.out.println("Hello world!");
     
 
-        
+        /* 
+         * Quan vulguis fer "push" al github et demanarà
+         * el nom de github i la contrasenya.
+         * 
+         * La contrasenya en realitat es el token que tens guardat a la carpeta
+         * de github en el disc dur, dins de M3
+        */
+
+
         // testeando los pushes
 
     }
